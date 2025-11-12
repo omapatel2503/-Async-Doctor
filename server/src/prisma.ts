4b437b8f187617e01f1685edc5140743127cf036
Divya Patil <dkp7112000@gmail.com>
2025-11-11 22:48:49 -0400

    Updated docker file

--- server/src/prisma.ts
+++ server/src/prisma.ts
@@ -1,4 +1,4 @@
 // Use the Prisma client generated to src/generated/prisma per schema.prisma
-import { PrismaClient } from './generated/prisma/client';
+import { PrismaClient } from '@prisma/client';
 
 export const prisma = new PrismaClient();
